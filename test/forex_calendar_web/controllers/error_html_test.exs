e6f71549d5ce4d8836c67b4dbb9554b77c0722cb
sophiathedev <thedevguy1337@gmail.com>
2025-06-18 15:22:49 +0700

    add: reformat

--- test/forex_calendar_web/controllers/error_html_test.exs
+++ test/forex_calendar_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule ForexCalendarWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(ForexCalendarWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(ForexCalendarWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
